5a42e3a7a5ff7b9c6c63a8ca70fc53b54345cb82
zomon34 <90390333+zomon34@users.noreply.github.com>
2023-09-27 12:20:47 +0200

    one less argument is needed to fire a bullet
    this solution is less than ideal, will try to reimplement it once I've created a class

--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -30,13 +30,13 @@ public class PlayerFire : MonoBehaviour
         if (Input.GetMouseButton(0) && timer >= fireRate)
         {
             timer = 0;
-            StartCoroutine(FireBullets(bulletsToFire));
+            StartCoroutine(FireBulletVolley(bulletsToFire));
         }
 
         timer += Time.deltaTime;
     }
 
-    IEnumerator FireBullets(int numberOfBullets)
+    IEnumerator FireBulletVolley(int numberOfBullets)
     {
         audioSource.pitch = Random.Range(0.75f, 1.25f);
         audioSource.Play();
@@ -48,17 +48,17 @@ public class PlayerFire : MonoBehaviour
         {
             if (numberOfBullets % 2 != 0)
             {
-                Vector3 newRotation = GenerateBulletRotationVector(i, playerZRotation, 3, -90);
+                Vector3 newRotation = GenerateBulletRotationVector(i, playerZRotation, -90);
                 Instantiate(laserPrefab, staffHeadPosition, Quaternion.Euler(newRotation));
             }
             else
             {
-                Vector3 newRotation = GenerateBulletRotationVector(i, playerZRotation, 3, -93);
+                Vector3 newRotation = GenerateBulletRotationVector(i, playerZRotation, -93);
                 Instantiate(laserPrefab, staffHeadPosition, Quaternion.Euler(newRotation));
 
                 i++;
 
-                newRotation = GenerateBulletRotationVector(i, playerZRotation, 3, -93);
+                newRotation = GenerateBulletRotationVector(i, playerZRotation, -93);
                 Instantiate(laserPrefab, staffHeadPosition, Quaternion.Euler(newRotation));
             }
             
@@ -66,15 +66,16 @@ public class PlayerFire : MonoBehaviour
         }
     }
 
-    // TODO: decrease the amount of arguments this needs (probably best solved with a new class)
+    // TODO: create a class that has this function or can be used as an argument
+    // Replace the hardcoded 3 with a variable that is sent in as an argument
     // Also add an explanation or make the code simpler for future me
-    Vector3 GenerateBulletRotationVector(int i, float zRotation, float angleVariance, float baseZ)
+    Vector3 GenerateBulletRotationVector(int i, float zRotation, float baseZ)
     {
-        float mod = i * angleVariance * 2;
+        float mod = i * 3 * 2;
         if (i % 2 == 0)
         {
-            mod = angleVariance * -1;
+            mod = 3 * -1;
         }
-        return new Vector3(0, 0, baseZ - i * angleVariance + mod + zRotation);
+        return new Vector3(0, 0, baseZ - i * 3 + mod + zRotation);
     }
 }
